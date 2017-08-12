ad733cbb98628ffd82d7c6ce9c2fa73415157b69
kentanvictor <2030457926@qq.com>
2017-08-13 00:00:48 +0800

    fifth commit

--- app/src/main/java/com/example/dell/datapage/MainActivity.java
+++ app/src/main/java/com/example/dell/datapage/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.dell.datapage;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +18,10 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private List<String> data = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    private int number = 20;//每次获取多少数据
+    private int maxPage = 5;//总共有多少页
+    private boolean loadFinish = true;//默认是否加载完成下一页
 
     @TargetApi(Build.VERSION_CODES.M)
     @Override
@@ -25,20 +31,51 @@ public class MainActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.list);
         listView.setOnScrollChangeListener(new ScrollListener());
         data.addAll(DataService.getData(0, 20));
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.listview_item, R.id.textView, data);
+        adapter = new ArrayAdapter<String>(this, R.layout.listview_item, R.id.textView, data);
+        /*listView.addFooterView();//添加页脚*/
         listView.setAdapter(adapter);
     }
 
     @TargetApi(Build.VERSION_CODES.M)
     private final class ScrollListener implements View.OnScrollChangeListener {
 
-        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-            Log.i("MainActivity", "OnScroll(firstVisibleItem=" + firstVisibleItem + ",visibleItemCount=" +
-                    visibleItemCount + ",totalItemCount=" + totalItemCount + ")");
-        }
-
         @Override
         public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
         }
+
+        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, final int totalItemCount) {
+            Log.i("MainActivity", "OnScroll(firstVisibleItem=" + firstVisibleItem + ",visibleItemCount=" +
+                    visibleItemCount + ",totalItemCount=" + totalItemCount + ")");
+            int lastItemid = listView.getLastVisiblePosition();//获取当前屏幕最后的Item的ID
+            if ((lastItemid + 1) == totalItemCount)//达到数据的最后一条记录
+            {
+                final int loadTotal = totalItemCount;
+                int currentPage = totalItemCount % number == 0 ? totalItemCount / number : totalItemCount / number + 1;
+                int nextPage = currentPage + 1;//获取下一页
+                if (nextPage <= maxPage && loadFinish) {
+                    loadFinish = false;
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                Thread.sleep(3000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            List<String> result = DataService.getData(totalItemCount, number);
+                            handler.sendMessage(handler.obtainMessage(100, result));
+                        }
+                    }).start();
+                }
+            }
+        }
     }
+
+    Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            data.addAll((List<String>) msg.obj);
+            adapter.notifyDataSetChanged();//告诉ListView数据已经发生改变,要求ListVIew更新界面显示
+            loadFinish = true;
+        }
+    };
 }
